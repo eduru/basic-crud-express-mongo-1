7738c063492b15f8b0f7a71bc90238dbc0640382
Eduardo Ruiz <eduru@me.com>
2021-09-15 13:47:21 +0900

    added route to add a new quote

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const Quote = require("./models/quotes");
+const { allowedNodeEnvironmentFlags } = require("process");
 
 mongoose.connect("mongodb://localhost:27017/LOTRquotes");
 const db = mongoose.connection;
@@ -22,6 +23,22 @@ app.set("views", path.join(__dirname, "views"));
 app.get("/quotes", async (req, res) => {
   const quotes = await Quote.find({});
   res.render("quotes/index", { quotes });
+});
+
+app.get("/quotes/new", async (req, res) => {
+  res.render("quotes/new");
+});
+
+app.post("/quotes", async (req, res) => {
+  const newQuote = await new Quote(req.body);
+  await newQuote.save();
+  res.redirect("/quotes");
+});
+
+app.get("/quotes/:id", async (req, res) => {
+  const { id } = await req.params;
+  const quote = await Quote.findById(id);
+  res.render("quotes/show", { quote });
 });
 
 app.listen(3000, () => {
