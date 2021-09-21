ea55fd81d159e2e0b3e7f2a955fe2efc369282f0
Eduardo Ruiz <eduru@me.com>
2021-09-21 11:47:33 +0900

    fixing patch route

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const methodOverride = require("method-override");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+
 const Quote = require("./models/quotes");
-const { allowedNodeEnvironmentFlags } = require("process");
 
 mongoose.connect("mongodb://localhost:27017/LOTRquotes");
 const db = mongoose.connection;
@@ -13,22 +13,16 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(methodOverride("_method"));
-app.use(express.urlencoded({ extended: true }));
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/quotes", async (req, res) => {
   const quotes = await Quote.find({});
   res.render("quotes/index", { quotes });
-});
-
-app.get("/quotes/:id", async (req, res) => {
-  const { id } = req.params;
-  const quote = await Quote.findById(id);
-  res.render("quotes/show", { quote });
 });
 
 app.get("/quotes/new", async (req, res) => {
@@ -39,6 +33,18 @@ app.post("/quotes", async (req, res) => {
   const newQuote = await new Quote(req.body);
   await newQuote.save();
   res.redirect("/quotes");
+});
+
+app.get("/quotes/:id", async (req, res) => {
+  const { id } = req.params;
+  const quote = await Quote.findById(id);
+  res.render("quotes/show", { quote });
+});
+
+app.get("/quotes/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const quote = await Quote.findById(id);
+  res.render("quotes/edit", { quote });
 });
 
 app.patch("/quotes/:id", async (req, res) => {
